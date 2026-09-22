73ce0a57c5fb4d1145bb6512e6bb16b83ab78f3c
Emiliano Elias <emilianoelias@Emilianos-MacBook-Pro.local>
2026-09-22 01:24:10 -0700

    develop env webapp deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import { AuthProvider } from './store/auth.jsx'
 import { AppointmentsProvider } from './store/appointments.jsx'
 import './index.css'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <AuthProvider>
         <AppointmentsProvider>
           <App />
